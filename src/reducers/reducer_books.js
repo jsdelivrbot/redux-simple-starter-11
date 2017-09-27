5bcfb26e341edd314437e715c04e451029eeeb62
pjliddy <pjliddy@gmail.com>
2017-09-27 17:15:01 -0400

    Create initial BooksReducer
    Returns hard-coded array of book objects with `title` property

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+// any file in project can call this reducer by exporting it
+export default function() {
+  return[
+    { title: 'JavaScript: The Good Parts'},
+    { title: 'Harry Potter'},
+    { title: 'Dark Tower'},
+    { title: 'Eloquent Ruby'}
+  ]
+}
