33f95e4f8f89297c21883891cc36e6dc0598d626
pjliddy <pjliddy@gmail.com>
2017-09-28 10:12:22 -0400

    Refactor BookList as container
    Add <BookList /> to app.js
    Import connect from react-redux into book_list.js
    Create mapStateToProps() function to return props object from state
    Export using connect(mapStateToProps) to make BookList a Redux container

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-// react-redux is the bridge between React and Redux
-import ReactRedux from 'react-redux';
+
+// import connect function from react-redux as bridge between React and Redux
+import { connect } from 'react-redux';
 
 
-// need to create a container for react-redux
-export default class BookList extends Component {
+// need to create a container with access to the Redux state through react-redux
+class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
@@ -21,3 +22,24 @@ export default class BookList extends Component {
     );
   }
 }
+
+/*
+  mapStateToProps()
+
+  Take state (array of books and active book) and return it as
+  props inside of BookList
+*/
+
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  }
+}
+
+/*
+  Export container for BookList created with react-redux connect() function.
+  The bject returned by mapStateToProps() becomes props for BookList.
+  When state updates, the BookList component will update props are re-render.
+*/
+
+export default connect(mapStateToProps)(BookList);
